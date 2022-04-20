87a6e9857fe28be545097df1966529e0753520a2
Selyanyn <56536978+Selyanyn@users.noreply.github.com>
2022-04-20 14:06:21 +0500

    8-20.04: implemented EmployeeAdapter

---
+++ app/src/main/java/com/example/a8_recycler_tiles/EmployeeAdapter.kt
@@ -0,0 +1,44 @@
+package com.example.a8_recycler_tiles
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+
+class EmployeeAdapter: RecyclerView.Adapter<EmployeeAdapter.EmployeesViewHolder>() {
+    class EmployeesViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        val nameTextView: TextView = itemView.findViewById(R.id.name)
+        val photoImageView: ImageView = itemView.findViewById(R.id.photo)
+        val departmentTextView: TextView = itemView.findViewById(R.id.department)
+        val deleteButtonView: Button = itemView.findViewById(R.id.delete_button)
+    }
+
+    private val employeesList = Employee.getMockEmployees()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EmployeeAdapter.EmployeesViewHolder {
+        val employeeElement = LayoutInflater.from(parent.context).inflate(R.layout.employee_item, parent, false)
+        return EmployeesViewHolder(employeeElement)
+    }
+
+    override fun onBindViewHolder(holder: EmployeeAdapter.EmployeesViewHolder, position: Int) {
+        val employee = employeesList[position]
+        with(holder) {
+            nameTextView.text = employee.fullName
+            departmentTextView.text = employee.department
+
+            Glide.with(photoImageView.context)
+                .load(employee.photoUrl)
+                .centerCrop()
+                .into(photoImageView)
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return employeesList.size
+    }
+
+}
